f5c0a2b4a1e0f317ff2d45aa6bad1369a5c66b55
evermake <vladeryabkin@gmail.com>
2024-09-30 01:55:13 +0300

    add light theme for `BookingTimeline`

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,15 @@
 <script setup lang="ts">
+import { usePreferredColorScheme } from '@vueuse/core'
+import { watch } from 'vue'
 import { RouterView } from 'vue-router'
+
+const scheme = usePreferredColorScheme()
+watch(scheme, (newScheme) => {
+  if (newScheme === 'dark')
+    document.documentElement.classList.add('dark')
+  else
+    document.documentElement.classList.remove('dark')
+}, { immediate: true })
 </script>
 
 <template>
